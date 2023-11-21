869ecb411f87ca502b5bd049fe441279f4a2c3d3
hj <2682094295@qq.com>
2023-11-21 22:06:40 +0800

    [记事本] 数据中心 增加移除方法

--- src/com/rhl/plugin/notepad/data/DataCentrer.java
+++ src/com/rhl/plugin/notepad/data/DataCentrer.java
@@ -19,12 +19,51 @@ public class DataCentrer {
      */
     public static String CURRENT_FILE_NAME = null;
     /**
-     *  当前的文件类型
+     * 当前的文件类型
      */
     public static String CURRENT_FILE_TYPE = null;
     /**
      * 笔记列表集合
      */
     public static List<NoteData> NOTE_LIST = new LinkedList<>();
+
+    /**
+     * 根据主键进行删除
+     * @param id
+     * @return
+     */
+    public static boolean remove(Long id) {
+        //判断当前中心里有没有数据
+        if (NOTE_LIST.isEmpty()) {
+            return Boolean.FALSE;
+        }
+        //根据ID查找当前元素
+        NoteData target = findById(id);
+
+        //没有找到直接返回 false
+        if (target == null){
+            return Boolean.FALSE;
+        }
+
+        // 移除
+        return NOTE_LIST.remove(target);
+    }
+
+    /**
+     * 根据ID进行查找
+     *
+     * @param id
+     * @return
+     */
+    private static NoteData findById(Long id) {
+        NoteData target = null;
+        for (NoteData note : NOTE_LIST) {
+            if (note.getId().equals(id)) {
+                target = note;
+                break;
+            }
+        }
+        return target;
+    }
 
 }
